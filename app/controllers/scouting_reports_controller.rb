bb03befca73c42ce8dc706da830e722803ff629a
Omarion Bitting <omarionbitt720@gmail.com>
2026-06-06 16:38:30 -0400

    Updated spacing in the array

--- app/controllers/scouting_reports_controller.rb
+++ app/controllers/scouting_reports_controller.rb
@@ -1,5 +1,5 @@
 class ScoutingReportsController < ApplicationController
-  before_action :set_scouting_report, only: [:show, :edit, :update, :destroy]
+  before_action :set_scouting_report, only: [ :show, :edit, :update, :destroy ]
   def index
     @scouting_reports = ScoutingReport.all
   end
